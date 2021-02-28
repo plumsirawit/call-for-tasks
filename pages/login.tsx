0c490f701c966c0f1653bc91343a319a532ebf5e
Sirawit <plurm2545@hotmail.com>
2021-03-01 03:18:41 +0700

    feat: initial login page

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import styles from "../styles/Login.module.css";
+import Link from "next/link";
+
+interface LoginBoxProps {}
+function LoginBox(props: LoginBoxProps) {
+  
+}
+
+export default function Login() {
+
+}
